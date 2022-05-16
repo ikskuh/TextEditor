e6368dcf5235ba40150b3b68a1ae9ddbb05fe9e0
Felix "xq" Queißner <git@masterq32.de>
2022-05-16 13:27:22 +0200

    Implements getText, setText, insertText, graphemeCount, init, deinit.

--- src/TextEditor.zig
+++ src/TextEditor.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
-const Zigstr = @import("zigstr");
+
+const ziglyph = @import("ziglyph");
+const CodePointIterator = ziglyph.CodePointIterator;
+const Grapheme = ziglyph.Grapheme;
+const GraphemeIterator = Grapheme.GraphemeIterator;
+
+// const Zigstr = @import("zigstr");
 
 const TextEditor = @This();
 
@@ -17,31 +23,37 @@ pub const EditUnit = enum {
     line,
 };
 
-data: Zigstr,
+pub const InsertError = std.mem.Allocator.Error || error{InvalidUtf8};
+
+bytes: std.ArrayList(u8),
 cursor: usize,
 
-pub fn init(allocator: std.mem.Allocator, initial_text: []const u8) !TextEditor {
-    _ = allocator;
-    std.debug.assert(std.unicode.utf8ValidateSlice(initial_text));
-    @panic("not implemented yet");
+pub fn init(allocator: std.mem.Allocator, initial_text: []const u8) InsertError!TextEditor {
+    if (!std.unicode.utf8ValidateSlice(initial_text))
+        return error.InvalidUtf8;
+    var str = try std.ArrayList(u8).initCapacity(allocator, initial_text.len);
+    str.appendSliceAssumeCapacity(initial_text);
+    var editor = TextEditor{ .bytes = str, .cursor = 0 };
+    editor.cursor = editor.graphemeCount();
+    return editor;
 }
 
 pub fn deinit(editor: *TextEditor) void {
+    editor.bytes.deinit();
     editor.* = undefined;
-    @panic("not implemented yet");
 }
 
 /// Replaces the contents of the editor with `text` and moves the cursor to the right.
-pub fn setText(editor: *TextEditor, text: []const u8) !void {
-    std.debug.assert(std.unicode.utf8ValidateSlice(text));
-    _ = editor;
-    _ = text;
-    @panic("not implemented yet");
+pub fn setText(editor: *TextEditor, text: []const u8) InsertError!void {
+    if (!std.unicode.utf8ValidateSlice(text))
+        return error.InvalidUtf8;
+    try editor.bytes.replaceRange(0, editor.bytes.items.len, text);
+    editor.cursor = editor.graphemeCount();
 }
 
 /// Returns the current text content.
-pub fn constSlice(editor: TextEditor) []const u8 {
-    return editor.data.bytes.items;
+pub fn getText(editor: TextEditor) []const u8 {
+    return editor.bytes.items;
 }
 
 /// Moves the cursor one `unit` into `direction`.
@@ -63,9 +75,52 @@ pub fn delete(editor: *TextEditor, direction: EditDirection, unit: EditUnit) voi
 }
 
 /// Inserts utf-8 encoded `text` at the cursor.
-pub fn insertText(editor: *TextEditor, text: []const u8) !void {
-    std.debug.assert(std.unicode.utf8ValidateSlice(text));
-    _ = editor;
-    _ = text;
-    @panic("not implemented yet");
+pub fn insertText(editor: *TextEditor, text: []const u8) InsertError!void {
+    if (!std.unicode.utf8ValidateSlice(text))
+        return error.InvalidUtf8;
+
+    const offset = editor.graphemeToByteOffset(editor.cursor);
+    try editor.bytes.insertSlice(offset, text);
+    editor.cursor += countGraphemes(text);
+}
+
+fn graphemeToByteOffset(editor: TextEditor, offset: usize) usize {
+    var iter = editor.graphemeIterator();
+
+    var i: usize = 0;
+    while (iter.next()) |gc| : (i += 1) {
+        if (i == offset) {
+            return gc.offset;
+        }
+    }
+
+    std.debug.assert(i == offset);
+    return editor.bytes.items.len;
+}
+
+/// Returns the number of graphemes in the input buffer.
+pub fn graphemeCount(editor: TextEditor) usize {
+    return countGraphemes(editor.bytes.items);
+}
+
+fn graphemeIterator(editor: TextEditor) GraphemeIterator {
+    return makeGraphemeIteratorUnsafe(editor.bytes.items);
+}
+
+/// Creates a new grapheme iterator assuming `string` is valid utf8
+fn makeGraphemeIteratorUnsafe(string: []const u8) GraphemeIterator {
+    return GraphemeIterator.init(string) catch |e| switch (e) {
+        // out editor guarantees that we always have valid utf8
+        error.InvalidUtf8 => unreachable,
+    };
+}
+
+/// Counts the graphemes in `string` asuming valid utf8.
+fn countGraphemes(string: []const u8) usize {
+    var i: usize = 0;
+    var iter = makeGraphemeIteratorUnsafe(string);
+    while (iter.next() != null) {
+        i += 1;
+    }
+    return i;
 }
--- src/testsuite.zig
+++ src/testsuite.zig
@@ -5,7 +5,7 @@ test "empty init" {
     var editor = try TextEditor.init(std.testing.allocator, "");
     defer editor.deinit();
 
-    try std.testing.expectEqualStrings("", editor.constSlice());
+    try std.testing.expectEqualStrings("", editor.getText());
     try std.testing.expect(editor.cursor == 0);
 }
 
@@ -13,6 +13,31 @@ test "preloaded init" {
     var editor = try TextEditor.init(std.testing.allocator, "[😊] Häuschen");
     defer editor.deinit();
 
-    try std.testing.expectEqualStrings("[😊] Häuschen", editor.constSlice());
+    try std.testing.expectEqualStrings("[😊] Häuschen", editor.getText());
     try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+}
+
+test "basic insert" {
+    var editor = try TextEditor.init(std.testing.allocator, "");
+    defer editor.deinit();
+
+    try editor.insertText("[😊");
+    try editor.insertText("] Hä");
+    try editor.insertText("uschen");
+
+    try std.testing.expectEqualStrings("[😊] Häuschen", editor.getText());
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+}
+
+test "setText" {
+    var editor = try TextEditor.init(std.testing.allocator, "");
+    defer editor.deinit();
+
+    try std.testing.expectEqualStrings("", editor.getText());
+    try std.testing.expect(editor.cursor == 0);
+
+    try editor.setText("[😊] Häuschen");
+
+    try std.testing.expectEqualStrings("[😊] Häuschen", editor.getText());
+    try std.testing.expect(editor.cursor == 12);
 }

3e176ec6a0556843c06e194055d1920fc2a69022
Felix "xq" Queißner <git@masterq32.de>
2022-05-16 14:03:25 +0200

    Implements setCursor, moveCursor.

--- src/TextEditor.zig
+++ src/TextEditor.zig
@@ -4,6 +4,8 @@ const ziglyph = @import("ziglyph");
 const CodePointIterator = ziglyph.CodePointIterator;
 const Grapheme = ziglyph.Grapheme;
 const GraphemeIterator = Grapheme.GraphemeIterator;
+const Word = @import("ziglyph").Word;
+const WordIterator = Word.WordIterator;
 
 // const Zigstr = @import("zigstr");
 
@@ -33,7 +35,10 @@ pub fn init(allocator: std.mem.Allocator, initial_text: []const u8) InsertError!
         return error.InvalidUtf8;
     var str = try std.ArrayList(u8).initCapacity(allocator, initial_text.len);
     str.appendSliceAssumeCapacity(initial_text);
-    var editor = TextEditor{ .bytes = str, .cursor = 0 };
+    var editor = TextEditor{
+        .bytes = str,
+        .cursor = 0,
+    };
     editor.cursor = editor.graphemeCount();
     return editor;
 }
@@ -56,13 +61,83 @@ pub fn getText(editor: TextEditor) []const u8 {
     return editor.bytes.items;
 }
 
+pub const SetCursorError = error{OutOfBounds};
+
+/// Moves the cursor to the grapheme `offset`. Allowed range is from `0` up to `graphemeCount()`.
+pub fn setCursor(editor: *TextEditor, offset: usize) SetCursorError!void {
+    const limit = editor.graphemeCount();
+    if (offset > limit)
+        return error.OutOfBounds;
+    editor.cursor = offset;
+}
+
 /// Moves the cursor one `unit` into `direction`.
 pub fn moveCursor(editor: *TextEditor, direction: EditDirection, unit: EditUnit) void {
-    //
-    _ = editor;
-    _ = direction;
-    _ = unit;
-    @panic("not implemented yet");
+    const byte_cursor = editor.graphemeToByteOffset(editor.cursor);
+    switch (direction) {
+        .left => {
+            if (editor.cursor == 0) // trivial case
+                return;
+
+            switch (unit) {
+                .line => editor.cursor = 0,
+                .word => {
+                    var iter = WordIterator.init(editor.bytes.items) catch |e| unreachableUtf8(e);
+
+                    var last_word: ?Word = null;
+                    while (iter.next()) |word| {
+                        if (word.offset >= byte_cursor) {
+                            break;
+                        }
+                        last_word = word;
+                    }
+
+                    if (last_word) |word| {
+                        editor.cursor = countGraphemes(editor.bytes.items[0..word.offset]);
+                    } else {
+                        // no last word means we're in the first word.
+                        // cursor full-throttle to the left
+                        editor.cursor = 0;
+                    }
+                },
+                .letter => {
+                    editor.cursor -= 1;
+                },
+            }
+        },
+        .right => {
+            const upper_limit = editor.graphemeCount();
+            if (editor.cursor == upper_limit) // trivial case
+                return;
+
+            switch (unit) {
+                // cursor to the end of the editing window
+                .line => editor.cursor = upper_limit,
+
+                // moving a word means we have to find a word boundary
+                .word => {
+                    const rest_string = editor.bytes.items[byte_cursor..];
+                    var iter = WordIterator.init(rest_string) catch |e| unreachableUtf8(e);
+
+                    // assume we're in a word right now, so let's skip that
+                    _ = iter.next();
+
+                    if (iter.next()) |word| {
+                        // advance grapheme count in the word
+                        editor.cursor += countGraphemes(rest_string[0..word.offset]);
+                    } else {
+                        // last word:
+                        // move the cursor to the right
+                        editor.cursor = upper_limit;
+                    }
+                },
+
+                // moving the cursor one letter right is easy:
+                // just skip one grapheme
+                .letter => editor.cursor += 1,
+            }
+        },
+    }
 }
 
 /// Deletes the `unit` based on the cursor into `direction`.
@@ -84,6 +159,17 @@ pub fn insertText(editor: *TextEditor, text: []const u8) InsertError!void {
     editor.cursor += countGraphemes(text);
 }
 
+/// Returns the number of graphemes in the input buffer.
+pub fn graphemeCount(editor: TextEditor) usize {
+    return countGraphemes(editor.bytes.items);
+}
+
+fn graphemeIterator(editor: TextEditor) GraphemeIterator {
+    return makeGraphemeIteratorUnsafe(editor.bytes.items);
+}
+
+/// Converts a grapheme offset into a byte offset. This is required to get indices
+/// into `.bytes` from editing operations.
 fn graphemeToByteOffset(editor: TextEditor, offset: usize) usize {
     var iter = editor.graphemeIterator();
 
@@ -98,21 +184,16 @@ fn graphemeToByteOffset(editor: TextEditor, offset: usize) usize {
     return editor.bytes.items.len;
 }
 
-/// Returns the number of graphemes in the input buffer.
-pub fn graphemeCount(editor: TextEditor) usize {
-    return countGraphemes(editor.bytes.items);
-}
-
-fn graphemeIterator(editor: TextEditor) GraphemeIterator {
-    return makeGraphemeIteratorUnsafe(editor.bytes.items);
-}
-
 /// Creates a new grapheme iterator assuming `string` is valid utf8
 fn makeGraphemeIteratorUnsafe(string: []const u8) GraphemeIterator {
-    return GraphemeIterator.init(string) catch |e| switch (e) {
-        // out editor guarantees that we always have valid utf8
-        error.InvalidUtf8 => unreachable,
-    };
+    return GraphemeIterator.init(string) catch |e| unreachableUtf8(e);
+}
+
+// out editor guarantees that we always have valid utf8
+fn unreachableUtf8(err: error{InvalidUtf8}) noreturn {
+    switch (err) {
+        else => unreachable,
+    }
 }
 
 /// Counts the graphemes in `string` asuming valid utf8.
--- src/testsuite.zig
+++ src/testsuite.zig
@@ -41,3 +41,150 @@ test "setText" {
     try std.testing.expectEqualStrings("[😊] Häuschen", editor.getText());
     try std.testing.expect(editor.cursor == 12);
 }
+
+test "setCursor" {
+    var editor = try TextEditor.init(std.testing.allocator, "[😊] Häuschen");
+    defer editor.deinit();
+
+    try editor.setCursor(0);
+    try std.testing.expectEqual(@as(usize, 0), editor.cursor);
+
+    try editor.setCursor(4);
+    try std.testing.expectEqual(@as(usize, 4), editor.cursor);
+
+    try editor.setCursor(12);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+
+    try std.testing.expectError(error.OutOfBounds, editor.setCursor(13));
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+}
+
+test "moveCursor (line)" {
+    var editor = try TextEditor.init(std.testing.allocator, "[😊] Häuschen");
+    defer editor.deinit();
+
+    try editor.setCursor(4);
+    editor.moveCursor(.left, .line);
+    try std.testing.expectEqual(@as(usize, 0), editor.cursor);
+    editor.moveCursor(.left, .line);
+    try std.testing.expectEqual(@as(usize, 0), editor.cursor);
+
+    editor.moveCursor(.right, .line);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+
+    try editor.setCursor(4);
+    editor.moveCursor(.right, .line);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+}
+
+test "moveCursor (letter)" {
+    var editor = try TextEditor.init(std.testing.allocator, "[😊] Häuschen");
+    defer editor.deinit();
+
+    try editor.setCursor(4);
+    editor.moveCursor(.left, .letter);
+    try std.testing.expectEqual(@as(usize, 3), editor.cursor);
+    editor.moveCursor(.left, .letter);
+    try std.testing.expectEqual(@as(usize, 2), editor.cursor);
+    editor.moveCursor(.left, .letter);
+    try std.testing.expectEqual(@as(usize, 1), editor.cursor);
+    editor.moveCursor(.left, .letter);
+    try std.testing.expectEqual(@as(usize, 0), editor.cursor);
+    editor.moveCursor(.left, .letter);
+    try std.testing.expectEqual(@as(usize, 0), editor.cursor);
+
+    editor.moveCursor(.right, .letter);
+    try std.testing.expectEqual(@as(usize, 1), editor.cursor);
+
+    try editor.setCursor(10);
+    editor.moveCursor(.right, .letter);
+    try std.testing.expectEqual(@as(usize, 11), editor.cursor);
+    editor.moveCursor(.right, .letter);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+    editor.moveCursor(.right, .letter);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+}
+
+test "moveCursor (word, right)" {
+    var editor = try TextEditor.init(std.testing.allocator, "[😊] Häuschen");
+    defer editor.deinit();
+
+    try editor.setCursor(3);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 4), editor.cursor);
+
+    try editor.setCursor(4);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+
+    try editor.setCursor(6);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+
+    try editor.setCursor(10);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 12), editor.cursor);
+
+    try editor.setText("void main foo bar");
+    try editor.setCursor(2);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 4), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 5), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 9), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 10), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 13), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 14), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 17), editor.cursor);
+    editor.moveCursor(.right, .word);
+    try std.testing.expectEqual(@as(usize, 17), editor.cursor);
+}
+
+test "moveCursor (word, left)" {
+    var editor = try TextEditor.init(std.testing.allocator, "[😊] Häuschen");
+    defer editor.deinit();
+
+    try editor.setCursor(12);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 4), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 3), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 2), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 1), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 0), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 0), editor.cursor);
+
+    try editor.setText("void main foo  bar");
+    try std.testing.expectEqual(@as(usize, 18), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 15), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 13), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 10), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 9), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 5), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 4), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 0), editor.cursor);
+    editor.moveCursor(.left, .word);
+    try std.testing.expectEqual(@as(usize, 0), editor.cursor);
+}

30e89438a33f0cfd3a81a9d096f70276e7f8fa5b
Felix "xq" Queißner <xq@random-projects.net>
2023-02-28 09:15:23 +0100

    Update to support package-manager.

--- build.zig
+++ build.zig
@@ -5,21 +5,26 @@ const pkgs = struct {
         .name = "ziglyph",
         .source = .{ .path = "vendor/ziglyph/src/ziglyph.zig" },
     };
-    const zigstr = std.build.Pkg{
-        .name = "zigstr",
-        .source = .{ .path = "vendor/zigstr/src/Zigstr.zig" },
-        .dependencies = &.{ziglyph},
-    };
 };
 
 pub fn build(b: *std.build.Builder) !void {
-    const mode = b.standardReleaseOptions();
+    b.addModule(.{
+        .name = "text-editor",
+        .source_file = .{ .path = "src/TextEditor.zig" },
+        .dependencies = &.{
+            .{ .name = "ziglyph", .module = b.dependency("ziglyph", .{}).module("ziglyph") },
+        },
+    });
+
+    const optimize = b.standardOptimizeOption(.{});
     const test_step = b.step("test", "Runs the test suite.");
 
-    const test_runner = b.addTest("src/testsuite.zig");
-    test_runner.setBuildMode(mode);
-    test_runner.addPackage(pkgs.ziglyph);
-    test_runner.addPackage(pkgs.zigstr);
+    const test_runner = b.addTest(.{
+        .root_source_file = .{ .path = "src/testsuite.zig" },
+        .optimize = optimize,
+        .target = .{},
+    });
+    test_runner.addModule("ziglyph", b.dependency("ziglyph", .{}).module("ziglyph"));
 
     test_step.dependOn(&test_runner.step);
 }

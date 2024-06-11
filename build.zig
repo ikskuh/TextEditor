19e13a78a4d47630520379319a407ee7c6ba1c97
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2024-06-11 23:22:52 +0200

    Update to Zig 0.13

--- build.zig
+++ build.zig
@@ -7,14 +7,14 @@ pub fn build(b: *std.Build) !void {
     const ziglyph = b.dependency("ziglyph", .{ .target = target, .optimize = optimize }).module("ziglyph");
 
     const text_editor = b.addModule("text-editor", .{
-        .root_source_file = .{ .path = "src/TextEditor.zig" },
+        .root_source_file = b.path("src/TextEditor.zig"),
         .target = target,
         .optimize = optimize,
     });
     text_editor.addImport("ziglyph", ziglyph);
 
     const test_runner = b.addTest(.{
-        .root_source_file = .{ .path = "src/testsuite.zig" },
+        .root_source_file = b.path("src/testsuite.zig"),
         .target = target,
         .optimize = optimize,
     });
